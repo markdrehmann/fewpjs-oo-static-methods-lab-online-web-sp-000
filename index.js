7d0419ff80305857b8e34259e0863adb268feb4f
markdrehmann <markdrehmann@gmail.com>
2021-04-05 11:12:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    string = this.capitalize(string);
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let array = string.split(" ")
+    array.forEach(function(el) {
+      if (exceptions.includes(el)) {
+        result.push(el)
+      } else {
+        result.push(Formatter.capitalize(el))
+      }
+    })
+    return result.join(" ")
+  }
 }
